ef4c608f77e2ec798c7b3499ba327f3faa44007a
Jaakko Mäenpää <jaakko.maenpaa@outlook.com>
2025-02-12 14:33:17 +0200

    Add cors

--- server/src/main.py
+++ server/src/main.py
@@ -1,7 +1,10 @@
 import routes
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 app.register_blueprint(routes.calendar_bp, url_prefix="/api/tournaments")
 app.register_blueprint(routes.tournament_bp, url_prefix="/api/tournament")

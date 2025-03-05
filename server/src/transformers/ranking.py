e9666ba21aa10cc7b2cbde53232214cd747c2fd1
Jaakko Mäenpää <jaakko.maenpaa@outlook.com>
2025-03-05 18:07:12 +0200

    Handle case where country model for ranking entry does not exist

--- server/src/transformers/ranking.py
+++ server/src/transformers/ranking.py
@@ -3,6 +3,9 @@ from config import FLAG_URL
 
 
 def add_flag_url_to_country(country: Dict[str, Any]):
+    if not country:
+        return country
+
     flag_name = country.get("flag_name_svg")
     country["flag_url_svg"] = FLAG_URL + flag_name
     return country
